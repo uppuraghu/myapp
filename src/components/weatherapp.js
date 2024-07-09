39e7b4ea0e11076ef065b0e879a71c3137e8391b
uppuraghu <uppuraghu21@gmail.com>
2024-07-09 11:34:10 +0530

    weather

--- src/components/weatherapp.js
+++ src/components/weatherapp.js
@@ -1,17 +1,5 @@
 import React, { useState } from 'react';
-// import Stacks from './Navigations/stack';
-// import ButtonComponent from './components/button/button';
 
-// Uncomment the following lines as needed
-// import From from './components/forms/uncontrolled/form';
-// import ControlledForm from './components/controlled/controlled';
-// import CircleComponent from './components/class-component/circleTask/circle';
-// import UseStateExample from './components/functionalHooks/useState/usestate';
-// import TodoComponent from './components/functionalHooks/useState/todo';
-// import ButtonComponentsClass from './components/class-component/button';
-// import TodoComponent from './components/class-component/todo/todo';
-// import { Heading1 } from './components/headings/headings';
-// import ProductListing from './components/class-component/productListing/productListing';
 
 const App = () => {
   const [city,setCity]=useState("")
