170c45b229c5948ae965bf7caf78dab77ff28d69
uppuraghu <uppuraghu21@gmail.com>
2024-07-24 12:59:33 +0530

    a1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-
-import React, { useState } from 'react';
-import ToggleButton from './components/DARK_LIGHT/dark';
+import {Raghu} from "./components/p1/a1"
 
 
 const App = () => {
-  const [isDark, setIsDark] = useState(false);
-
-  const toggleBackground = () => {
-    setIsDark(!isDark);
-  };
-
-  const appStyle = {
-    
-    height: '100vh',
-    backgroundColor: isDark ? 'black' : 'white',
-    color: isDark ? 'white' : 'black',
-    
-  };
+  
 
   return (
-    <div style={appStyle} >
-            <ToggleButton Background={toggleBackground}/>
+    <div  >
+         <Raghu />  
     </div>
   );
 };
---
+++ src/components/p1/a1.js
@@ -0,0 +1,24 @@
+import  {useState} from "react";
+export function Raghu(){
+    const [value , updateValue] =useState(
+        {name:"raghu",
+         friend:"chintu",
+         lover:"paamu"
+    })
+    function updateSet(){
+        updateValue({
+            name:"rp",
+            friend:"oc",
+            lover:"mc"
+        })
+    }                                       
+
+    return(
+    <div>
+    <h1>
+        my name is {value.name}  and my  friend name   is {value.friend} and lover namr is {value.lover}
+    </h1>
+    <button onClick={updateSet} value={value} name="UPDATE">UPDATE</button>
+    </div>
+    )
+}
